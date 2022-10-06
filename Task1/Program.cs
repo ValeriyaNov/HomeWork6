86031c21579f5cb3e64b9676c5bcbbf1e854bc18
Valeriya <wal.now@yandex.ru>
2022-10-06 18:54:45 +0300

    T1

---
+++ Task1/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько 
+//чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3
+using static System.Console;
+Clear();
+WriteLine("Введите несколько чисел через пробел: ");
+string[] parameters=ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+int number=NumPositive(parameters);
+WriteLine(number);
+
+
+
+
+int NumPositive(string[] Array)
+{
+    int count=0;
+    for (int i=0; i<Array.Length; i++)
+    {
+        if (int.Parse(Array[i])>0) count++; 
+    }
+    return count;
+}
